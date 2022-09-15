07a1230ab06a13a72a219e9e7b742b4ea93d6a95
KotenkoNata <kotenkonata8@gmail.com>
2022-09-15 20:56:50 +0300

    feat: API integration

--- src/Dictionary/Dictionary.js
+++ src/Dictionary/Dictionary.js
@@ -1,18 +1,28 @@
 import React, {useState} from 'react';
+import axious from 'axios';
 import './Dictionary.css';
+
 
 const Dictionary = () => {
 
   const [keyword, setKeyword] = useState('');
 
+  function handleResponse(response) {
+    console.log(response.data[0])
+  }
+
   function search(e) {
     e.preventDefault();
-    alert(`Searching for ${keyword}`)
+
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axious.get(apiUrl).then(handleResponse);
   }
 
   function updateKeywordChange(e) {
     setKeyword(e.target.value);
   }
+
+
 
 return (
   <>
